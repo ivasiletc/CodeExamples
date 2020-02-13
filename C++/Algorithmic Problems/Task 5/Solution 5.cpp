29ccbba628872577949b7b1943de051c75e73f41
Iaroslav Vasiletc <vasilets.jar@gmail.com>
2020-02-13 18:36:52 +0100

    Changed folder structure

--- C++/Algorithmic Problems/Task 5/Solution 5.cpp
+++ C++/Algorithmic Problems/Task 5/Solution 5.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-struct City
+struct City 
 {
     int gasStation
     int distance;
@@ -14,10 +14,10 @@ int main() {
     char* line = new char[100000000];
     read(0, line, 100000000);
 
-    long long citiesNumber = 0, fuel = 0, maxFuel = 0, tmp;
+    long long citiesNumber = 0, fuel = 0, maxpal = 0, tmp;
     int iter = 0;
 
-    while (*line >= 48)
+    while (*line >= 48) 
     {
         citiesNumber = (citiesNumber << 3) + (citiesNumber << 1) + *line - '0';
         line++;
@@ -29,16 +29,16 @@ int main() {
     int index;
     int i = 0;
 
-    for (; i < citiesNumber; ++i)
+    for (; i < citiesNumber; ++i) 
     {
-        while (*line >= 48)
+        while (*line >= 48) 
         {
             cities[i].gasStation = (cities[i].gasStation << 3) + (cities[i].gasStation << 1) + *line - '0';
             line++;
         }
         line++;
-
-        while (*line >= 48)
+        
+        while (*line >= 48) 
         {
             cities[i].distance = (cities[i].distance << 3) + (cities[i].distance << 1) + *line - '0';
             line++;
@@ -46,42 +46,42 @@ int main() {
         line++;
 
         fuel = fuel + cities[i].gasStation - cities[i].distance;
-        if (fuel < maxFuel)
+        if (fuel < maxpal) 
         {
-            maxFuel = fuel;
+            maxpal = fuel;
             index = i + 1;
         }
     }
 
-    if (index >= citiesNumber)
+    if (index >= citiesNumber) 
     {
         index = 0;
     }
 
-    maxFuel = 0;
+    maxpal = 0;
     fuel = 0;
     i = index;
-
-    while (true)
+    
+    while (true) 
     {
         fuel = fuel + cities[i].gasStation - cities[i].distance;
 
-        if (fuel > maxFuel)
+        if (fuel > maxpal) 
         {
-            maxFuel = fuel;
+            maxpal = fuel;
         }
         i++;
-
-        if (i >= citiesNumber)
+        
+        if (i >= citiesNumber) 
         {
             i = 0;
         }
-
-        if (i == index)
+        
+        if (i == index) 
         {
             break;
         }
     }
 
-    printf("%i %lli", index, maxFuel);
+    printf("%i %lli", index, maxpal);
 }
