e611478973deaed6ead0234a186de198c7cd5b64
Iaroslav Vasiletc <vasilets.jar@gmail.com>
2020-02-13 18:40:48 +0100

    Update Solution 3.cpp
    Trailing spaces

--- C++/Algorithmic Problems/Task 3/Solution 3.cpp
+++ C++/Algorithmic Problems/Task 3/Solution 3.cpp
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     int tmp = 0;
     char* line = new char[15000000];
     read(0, line, 150000000);
@@ -14,13 +15,13 @@ int main() {
     int row = 0;
     int subMatrix = 0;
 
-    while (line[index] > 32) 
+    while (line[index] > 32)
     {
         row = (row << 3) + (row << 1) + line[index] - 48;
         index++;
     }
     index++;
-    while (line[index] > 32) 
+    while (line[index] > 32)
     {
         subMatrix = (subMatrix << 3) + (subMatrix << 1) + line[index] - 48;
         index++;
@@ -33,19 +34,19 @@ int main() {
     {
         matrix[i] = new int[row + 1];
     }
-    
-    for (int i = 1; i < row + 1; ++i) 
+
+    for (int i = 1; i < row + 1; ++i)
     {
-        for (int j = 1; j < row + 1; ++j) 
+        for (int j = 1; j < row + 1; ++j)
         {
             isNegative = false;
-            if (line[index] == 10) 
+            if (line[index] == 10)
             {
                 index++;
             }
-            while (line[index] > 32) 
+            while (line[index] > 32)
             {
-                if (line[index] == '-') 
+                if (line[index] == '-')
                 {
                     isNegative = true;
                     index++;
@@ -59,7 +60,7 @@ int main() {
 
             }
 
-            if (isNegative == true) 
+            if (isNegative == true)
             {
                 tmp = ~tmp + 1;
             }
@@ -76,13 +77,13 @@ int main() {
     {
         bob[i] = new int[4];
     }
-    
-    for (int l = 0; l < subMatrix; ++l) 
+
+    for (int l = 0; l < subMatrix; ++l)
     {
-        for (int i = 0; i < 4; ++i) 
+        for (int i = 0; i < 4; ++i)
         {
 
-            while (line[index] > 32) 
+            while (line[index] > 32)
             {
 
                 tmp = (tmp << 3) + (tmp << 1) + line[index] - 48;
@@ -97,7 +98,7 @@ int main() {
     int totalSum = 0;
     int sum = 0;
     map<int, int> map;
-    for (int i = 0; i < subMatrix; i++) 
+    for (int i = 0; i < subMatrix; i++)
     {
         sum =
             matrix[bob[i][2] + 1][bob[i][3] + 1] +
@@ -111,13 +112,13 @@ int main() {
     int result = 0;
     tmp = 0;
 
-    for (std::map<int, int>::iterator it = map.begin(); it != map.end(); ++it) 
+    for (std::map<int, int>::iterator it = map.begin(); it != map.end(); ++it)
     {
         if (it->second == tmp)
         {
             result++;
         }
-        else if (it->second > tmp) 
+        else if (it->second > tmp)
         {
             result = 1;
             tmp = it->second;
