df25094a2842465921a2a7c665ecca32183ff912
Iaroslav Vasiletc <vasilets.jar@gmail.com>
2020-02-13 18:41:47 +0100

    Update Solution 2.cpp
    if, else, function code blocks starting from new line

--- C++/Algorithmic Problems/Task 2/Solution 2.cpp
+++ C++/Algorithmic Problems/Task 2/Solution 2.cpp
@@ -7,60 +7,76 @@ Node* currentNode;
 char result[65];
 char tmpWord[65];
 //Tree
-struct Node {
+struct Node
+{
     char value;
     Node* right;
     Node* left;
     Node* parent;
 
     //Algorhytm
-    void search() {
-        if (this->left) {
+    void search()
+    {
+        if (this->left)
+        {
             this->left->search();
         }
-        if (this->right) {
+
+        if (this->right)
+        {
             this->right->search();
         }
-        else if (!this->left) {
+        else if (!this->left)
+        {
             currentNode = this;
             int i = 0;
-            while (currentNode->parent) {
+            while (currentNode->parent)
+            {
                 tmpWord[i] = currentNode->value;
                 currentNode = currentNode->parent;
                 ++i;
             }
             tmpWord[i] = currentNode->value;
             tmpWord[i + 1] = 0;
-            if (strcmp(result, tmpWord) < 0) {
+
+            if (strcmp(result, tmpWord) < 0)
+            {
                 strncpy(result, tmpWord, 65);
             }
         }
     }
 };
 
-int main() {
+int main()
+{
     char tmpChar;
     char lastChar = getchar_unlocked();
 
     Node* start = new Node();
     currentNode = start;
 
-    while (tmpChar != EOF) {
+    while (tmpChar != EOF)
+    {
         tmpChar = getchar_unlocked();
-        if (tmpChar >= 'a' && tmpChar <= 'z') {
+        if (tmpChar >= 'a' && tmpChar <= 'z')
+        {
             currentNode->value = lastChar;
             currentNode = start;
             lastChar = tmpChar;
         }
-        else if (tmpChar == 'R') {
-            if (!currentNode->right) {
+        else if (tmpChar == 'R')
+        {
+            if (!currentNode->right)
+            {
                 currentNode->right = new Node();
                 currentNode->right->parent = currentNode;
             }
             currentNode = currentNode->right;
         }
-        else if (tmpChar == 'L') {
-            if (!currentNode->left) {
+        else if (tmpChar == 'L')
+        {
+            if (!currentNode->left)
+            {
                 currentNode->left = new Node();
                 currentNode->left->parent = currentNode;
             }
