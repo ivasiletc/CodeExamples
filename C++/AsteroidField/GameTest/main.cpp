c9ccce4d573bb2697f2a98cf486de10a31c20159
Iaroslav Vasiletc <vasilets.jar@gmail.com>
2020-03-01 18:06:40 +0100

    Update files

--- C++/AsteroidField/GameTest/main.cpp
+++ C++/AsteroidField/GameTest/main.cpp
@@ -1,9 +1,11 @@
 #include <SFML/graphics.hpp>
 #include <iostream>
 
+#include "logger.h"
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML TEST");
+    sf::RenderWindow window(sf::VideoMode(500, 700), "Asteroid Field game by Iaroslav Vasiletc");
 
     while (window.isOpen())
     {
@@ -17,6 +19,7 @@ int main()
         //draw SFML objects
 
         window.display();
+        log << "Hello world " << 9;
     }
 
     return EXIT_SUCCESS;
