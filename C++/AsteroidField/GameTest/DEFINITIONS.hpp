01392e146945171b7839b7a423dbf9d22b8c7571
Iaroslav Vasiletc <vasilets.jar@gmail.com>
2020-03-09 14:42:29 +0100

    Create DEFINITIONS.hpp

---
+++ C++/AsteroidField/GameTest/DEFINITIONS.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 800
+
+#define SPLASH_STATE_SHOW_TIME 3.0
+
+#define SPLASH_SCENE_BACKGROUND_FILEPATH "Resource Files/background1.png"
+#define MAIN_MENU_BACKGROUND_FILEPATH "Resource Files/background3.png"
+#define GAME_TITLE_FILEPATH "Resource Files/background2.png"
+#define PLAY_BUTTON_FILEPATH "Resource Files/background1.png"
+
