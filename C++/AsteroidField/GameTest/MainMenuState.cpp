d2e1dc4102b47ac489de9671cfabbe501a876259
Iaroslav Vasiletc <vasilets.jar@gmail.com>
2020-03-10 17:37:41 +0100

    Update MainMenuState.cpp

--- C++/AsteroidField/GameTest/MainMenuState.cpp
+++ C++/AsteroidField/GameTest/MainMenuState.cpp
@@ -5,6 +5,7 @@
 
 #include "MainMenuState.hpp"
 #include "DEFINITIONS.hpp"
+#include "GameState.hpp"
 
 MainMenuState::MainMenuState(GameDataRef data) : _data(data)
 {
@@ -18,8 +19,11 @@ void MainMenuState::Init()
     _data->assets.LoadTexture("Play Button", PLAY_BUTTON_FILEPATH);
 
     _background.setTexture(this->_data->assets.GetTexture("Main Menu Background"));
-    _background.setTexture(this->_data->assets.GetTexture("Game Title"));
-    _background.setTexture(this->_data->assets.GetTexture("Play Button"));
+    _title.setTexture(this->_data->assets.GetTexture("Game Title"));
+    _playButton.setTexture(this->_data->assets.GetTexture("Play Button"));
+
+    _title.setPosition((SCREEN_WIDTH / 2) - (_title.getGlobalBounds().width / 2), _title.getGlobalBounds().height / 2);
+    _playButton.setPosition((SCREEN_WIDTH / 2) - (_playButton.getGlobalBounds().width / 2), (SCREEN_HEIGHT / 2) - (_playButton.getGlobalBounds().height / 2));
 }
 
 void MainMenuState::HandleInput()
@@ -31,6 +35,12 @@ void MainMenuState::HandleInput()
         if (sf::Event::Closed == event.type)
         {
             _data->window.close();
+        }
+
+        if (this->_data->input.IsSpriteClicked(this->_playButton, sf::Mouse::Left, this->_data->window))
+        {
+            // Switch To Main Menu
+            this->_data->machine.AddState(StateRef(new GameState(_data)), true);
         }
     }
 }
@@ -45,6 +55,8 @@ void MainMenuState::Draw(float dt)
     _data->window.clear();
 
     _data->window.draw(_background);
+    _data->window.draw(this->_title);
+    _data->window.draw(this->_playButton);
 
     _data->window.display();
 }
