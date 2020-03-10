4805713f2d1cc327b1946dd435fed21a26f3e1f0
Iaroslav Vasiletc <vasilets.jar@gmail.com>
2020-03-10 17:37:14 +0100

    Project files update

--- C++/AsteroidField/GameTest/DEFINITIONS.hpp
+++ C++/AsteroidField/GameTest/DEFINITIONS.hpp
@@ -1,12 +1,27 @@
 #pragma once
 
+/*   Game settings   */
 #define SCREEN_WIDTH 600
 #define SCREEN_HEIGHT 800
 
 #define SPLASH_STATE_SHOW_TIME 3.0
 
+#define SPACE_MOVEMENT_SPEED 20.0f
+/*   #############   */
+
+
+
+
+/*   Textures path   */
 #define SPLASH_SCENE_BACKGROUND_FILEPATH "Resource Files/background1.png"
 #define MAIN_MENU_BACKGROUND_FILEPATH "Resource Files/background3.png"
 #define GAME_TITLE_FILEPATH "Resource Files/background2.png"
 #define PLAY_BUTTON_FILEPATH "Resource Files/background1.png"
+/*   #############   */
 
+enum GameStates
+{
+	eReady,
+	ePlaying,
+	eGameOver
+};
