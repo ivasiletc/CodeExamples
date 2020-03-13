0b97a2503c3798ca9fa4e8f10f5c8742a18d5266
Iaroslav Vasiletc <vasilets.jar@gmail.com>
2020-03-13 17:30:37 +0100

    Update MainMenuState.cpp

--- C++/AsteroidField/GameTest/MainMenuState.cpp
+++ C++/AsteroidField/GameTest/MainMenuState.cpp
@@ -15,14 +15,11 @@ MainMenuState::MainMenuState(GameDataRef data) : _data(data)
 void MainMenuState::Init()
 {
     _data->assets.LoadTexture("Main Menu Background", MAIN_MENU_BACKGROUND_FILEPATH);
-    //_data->assets.LoadTexture("Game Title", GAME_TITLE_FILEPATH);
     _data->assets.LoadTexture("Play Button", PLAY_BUTTON_FILEPATH);
 
     _background.setTexture(this->_data->assets.GetTexture("Main Menu Background"));
-    //_title.setTexture(this->_data->assets.GetTexture("Game Title"));
     _playButton.setTexture(this->_data->assets.GetTexture("Play Button"));
 
-    //_title.setPosition((SCREEN_WIDTH / 2) - (_title.getGlobalBounds().width / 2), _title.getGlobalBounds().height / 2);
     _playButton.setPosition((SCREEN_WIDTH / 2) - (_playButton.getGlobalBounds().width / 2), (SCREEN_HEIGHT / 2) - (_playButton.getGlobalBounds().height / 2));
 }
 
@@ -39,7 +36,7 @@ void MainMenuState::HandleInput()
 
         if (this->_data->input.IsSpriteClicked(this->_playButton, sf::Mouse::Left, this->_data->window))
         {
-            // Switch To Main Menu
+            // Switch To Game
             this->_data->machine.AddState(StateRef(new GameState(_data)), true);
         }
     }
