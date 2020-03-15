4329b6f2256f2056444a1cc84c2717aff103ae8d
Iaroslav Vasiletc <vasilets.jar@gmail.com>
2020-03-15 15:45:54 +0100

    Update Asteroid class
    Implement explosion logic

--- C++/AsteroidField/GameTest/Asteroid.cpp
+++ C++/AsteroidField/GameTest/Asteroid.cpp
@@ -8,6 +8,16 @@ Asteroid::Asteroid(GameDataRef data) : _data(data)
 	_spaceWidth = SCREEN_WIDTH;
 	_spaceHeight = SCREEN_HEIGHT;
 
+	animationFrames.push_back(_data->assets.GetTexture("Explosion frame 0"));
+	animationFrames.push_back(_data->assets.GetTexture("Explosion frame 1"));
+	animationFrames.push_back(_data->assets.GetTexture("Explosion frame 2"));
+	animationFrames.push_back(_data->assets.GetTexture("Explosion frame 3"));
+	animationFrames.push_back(_data->assets.GetTexture("Explosion frame 4"));
+	animationFrames.push_back(_data->assets.GetTexture("Explosion frame 5"));
+	animationFrames.push_back(_data->assets.GetTexture("Explosion frame 6"));
+	animationFrames.push_back(_data->assets.GetTexture("Explosion frame 7"));
+
+	_randomDirection = -50.0f;
 	_asteroidSpawnXOffset = 0;
 }
 
@@ -27,26 +37,76 @@ void Asteroid::SpawnAsteroid()
 	sprite.setScale(AsteroidRandomSize, AsteroidRandomSize);
 	sprite.setPosition(_asteroidSpawnXOffset, 0 - sprite.getGlobalBounds().height);
 	log << "Asteroid was spawned at X: " << _asteroidSpawnXOffset << " Y: " << 0 - sprite.getGlobalBounds().height;
-	asteroidSprites.push_back(sprite);
+
+	_randomDirection += 0.01f;
+	if (_randomDirection > 50.0f)
+	{
+		_randomDirection = -50.0f;
+	}
+
+	AsteroidSprite as;
+	int tmp = rand() % 100;
+
+	float rotationVelocity = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);;
+
+	if (tmp < 40)
+	{
+		as = { sprite, -(_randomDirection * 0.01f), rotationVelocity, 0.0f};
+	}
+	else if(tmp < 80)
+	{
+		as = { sprite, (_randomDirection * 0.01f), -rotationVelocity, 0.0f};
+	}
+	else
+	{
+		as = { sprite, 0, -0.2f*rotationVelocity, 0.0f };
+	}
+
+	asteroidSprites.push_back(as);
 }
 
 void Asteroid::MoveAsteroids(float dt)
 {
 	for ( int i = 0; i < asteroidSprites.size(); i++)
 	{
-		if (asteroidSprites.at(i).getPosition().x < 0 - asteroidSprites.at(i).getLocalBounds().width			 ||
-			asteroidSprites.at(i).getPosition().x > SCREEN_WIDTH + asteroidSprites.at(i).getLocalBounds().width  ||
-			asteroidSprites.at(i).getPosition().y > SCREEN_HEIGHT + asteroidSprites.at(i).getLocalBounds().height)
+		if (asteroidSprites.at(i).asteroidSprite.getPosition().x < 0 - asteroidSprites.at(i).asteroidSprite.getLocalBounds().width			 ||
+			asteroidSprites.at(i).asteroidSprite.getPosition().x > SCREEN_WIDTH + asteroidSprites.at(i).asteroidSprite.getLocalBounds().width  ||
+			asteroidSprites.at(i).asteroidSprite.getPosition().y > SCREEN_HEIGHT + asteroidSprites.at(i).asteroidSprite.getLocalBounds().height)
 		{
-			asteroidSprites.erase( asteroidSprites.begin( ) + i );
-			log << "Asteroid was deleted";
+			DeleteAsteroid(i);
+			log << "Asteroid was deleted (outside screen)";
 		}
 		else
 		{
-			sf::Vector2f position = asteroidSprites.at(i).getPosition();
+			sf::Vector2f position = asteroidSprites.at(i).asteroidSprite.getPosition();
 			float movement = ASTEROID_MOVEMENT_SPEED * dt;
 
-			asteroidSprites.at(i).move(0, movement);
+			asteroidSprites.at(i).asteroidSprite.move(asteroidSprites.at(i).direction, movement);
+			asteroidSprites.at(i).asteroidSprite.setRotation(asteroidSprites.at(i).defaultRotationPosition += asteroidSprites.at(i).rotationVelocity);
+		}
+	}
+}
+
+void Asteroid::MoveExplosions(float dt)
+{
+	for (unsigned short int i = 0; i < explosionSprites.size(); i++)
+	{
+		float movement = (SPACESHIP_FORWARD_MOVEMENT_SPEED + ASTEROID_MOVEMENT_SPEED) * dt;
+
+		if (explosionSprites.at(i).clock.getElapsedTime().asSeconds() > ASTEROID_EXPLOSION_DURATION / animationFrames.size())
+		{
+			explosionSprites.at(i).explosionSprite.setTexture(animationFrames.at(explosionSprites.at(i).animationIterator));
+			explosionSprites.at(i).explosionSprite.setScale(explosionSprites.at(i).scale + 0.6f, explosionSprites.at(i).scale + 0.6f);
+			explosionSprites.at(i).explosionSprite.setRotation(explosionSprites.at(i).rotation);
+			explosionSprites.at(i).explosionSprite.move(0, movement);
+
+			explosionSprites.at(i).clock.restart();
+			explosionSprites.at(i).animationIterator++;
+
+			if (explosionSprites.at(i).animationIterator == animationFrames.size())
+			{
+				explosionSprites.erase(explosionSprites.begin() + i);
+			}
 		}
 	}
 }
@@ -55,7 +115,15 @@ void Asteroid::DrawAsteroids()
 {
 	for (unsigned short int i = 0; i < asteroidSprites.size(); i++)
 	{
-		this->_data->window.draw(asteroidSprites.at(i));
+		this->_data->window.draw(asteroidSprites.at(i).asteroidSprite);
+	}
+}
+
+void Asteroid::DrawExplosions()
+{
+	for (unsigned short int i = 0; i < explosionSprites.size(); i++)
+	{
+		this->_data->window.draw(explosionSprites.at(i).explosionSprite);
 	}
 }
 
@@ -69,7 +137,25 @@ void Asteroid::DeleteAsteroid(int index)
 	asteroidSprites.erase(asteroidSprites.begin() + index);
 }
 
-const std::vector<sf::Sprite> &Asteroid::GetSprites() const
+void Asteroid::DestroyAsteroid(int index)
+{
+	try
+	{
+		sf::Sprite sprite(this->_data->assets.GetTexture("Explosion frame 0"));
+		sprite.setPosition(asteroidSprites.at(index).asteroidSprite.getPosition().x, asteroidSprites.at(index).asteroidSprite.getPosition().y);
+		ExplosionSprite es = { sprite, 0, asteroidSprites.at(index).asteroidSprite.getScale().x, asteroidSprites.at(index).asteroidSprite.getRotation() };
+		explosionSprites.push_back(es);
+	}
+	catch (const std::exception & e)
+	{
+		log_err << "Trying to get asteroid position from no longer existing asteroid";
+	}
+
+	DeleteAsteroid(index);
+	log << "Asteroid with index " << index << "was destroyed";
+}
+
+const std::vector<AsteroidSprite> &Asteroid::GetSprites() const
 {
 	return asteroidSprites;
 }
--- C++/AsteroidField/GameTest/Asteroid.hpp
+++ C++/AsteroidField/GameTest/Asteroid.hpp
@@ -4,6 +4,23 @@
 #include "Game.hpp"
 #include <vector>
 
+struct AsteroidSprite
+{
+	sf::Sprite asteroidSprite;
+	float direction;
+	float rotationVelocity;
+	float defaultRotationPosition;
+};
+
+struct ExplosionSprite
+{
+	sf::Sprite explosionSprite;
+	unsigned int animationIterator;
+	float scale;
+	float rotation;
+	sf::Clock clock;
+};
+
 class Asteroid
 {
 public:
@@ -12,17 +29,29 @@ public:
 	void SpawnAsteroid();
 
 	void MoveAsteroids(float dt);
+	void MoveExplosions(float dt);
+
 	void DrawAsteroids();
+	void DrawExplosions();
+
 	void RandomiseAsteroidOffset();
 	void DeleteAsteroid(int index);
+	void DestroyAsteroid(int index);
 
-	const std::vector<sf::Sprite>& GetSprites() const;
+	const std::vector<AsteroidSprite>& GetSprites() const;
 
 private:
 	GameDataRef _data;
-	std::vector<sf::Sprite> asteroidSprites;
+
+	std::vector<AsteroidSprite> asteroidSprites;
+	std::vector<ExplosionSprite> explosionSprites;
+
+	std::vector<sf::Texture> animationFrames;
 
 	int _spaceWidth;
 	int _spaceHeight;
 	int _asteroidSpawnXOffset;
+
+	float _randomDirection;
+
 };
